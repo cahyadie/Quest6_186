c24c795dd324b5c4497e8579ab6402bf54c56bc5
cahyadie <cahyadiilham12@gmail.com>
2024-11-22 17:32:23 +0700

    menambahkan fun setMatakuliah

--- app/src/main/java/com/example/quest6_186/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/quest6_186/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.quest6_186.ui.view.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.quest6_186.model.RencanaStudi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -10,5 +11,8 @@ class RencanaStudyViewModel : ViewModel() {
     private val _krsState = MutableStateFlow(RencanaStudi())
     val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
 
+    fun setMatakuliah(mkPilihan: String) {
+        _krsState.update {stateMK -> stateMK.copy(namaMK = mkPilihan)}
+    }
 }
 
